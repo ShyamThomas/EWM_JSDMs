4974fe06e6c423c4344c6c73ab4f24efe5cda07e
Shyam <thomas.shyam@gmail.com>
2021-07-20 13:45:42 -0500

    Update JSDM_trial.R

--- JSDM_trial.R
+++ JSDM_trial.R
@@ -1,8 +1,80 @@
+library(tidyverse)
 library(Hmsc)
 library(snow)
 library(miceadds)
 
-setwd("./JSDM")
+###folderpath "/Users/thom7552/UMNpostdoc/ProjectEWM/RProjects/JSDMs_hmsc"
+###All data in the Data folder
+
+###First load the entire JSDM dataset which contains year, lake ids, lake covariates, survey and sampling ids, and species presence absence (1/0) 
+JSDM.data=read_csv("Data/JSDM.newdata.csv")
+JSDM.data
+colnames(JSDM.data)[20]
+
+### Created a sub-species dataset that shows only species with lake level prevalence 10 percent and above
+Taxa_10Per=read_csv("Data/Taxa_10PerAbove.csv")
+Taxa_10Per
+TaxaNames_10Per=Taxa_10Per$Taxa
+TaxaNames_10Per
+### Subset JSDM dataset with the taxa of pervelance 10 percent and higher
+JSDM.subdata=JSDM.data%>%select(c(1:20),all_of(TaxaNames_10Per))
+JSDM.subdata
+
+### A seperate df showing all 8 covariates and their names
+Xdata=as.data.frame(cbind(JSDM.subdata$DEPTH_MAX,JSDM.subdata$avgSECCHI,JSDM.subdata$Road_Density,JSDM.subdata$Littoral,
+                          JSDM.subdata$GDD_WTR_10c,JSDM.subdata$Depth_mts,JSDM.subdata$DD,JSDM.subdata$CummGLD))
+colnames(Xdata)=c("MaxDepth", "AvgSecchi","RoadDensity","Littoral","SurfGDD","WithinLakeDepth","WithinLakeDegreeDays",
+                  "WithinLakeGLD")
+tail(Xdata)
+dim(Xdata)
+
+### A quick assessment of correlation among covariates
+library(corrplot)
+Xdata.cor=cor(Xdata, method="pearson")
+Xdata.cor
+corrplot.mixed(Xdata.cor, lower.col = "black", tl.pos = "l")
+
+### The fixed effects formula that goes into the JSDM model structure
+Xformula = ~MaxDepth+AvgSecchi+RoadDensity+Littoral+SurfGDD+WithinLakeDepth+WithinLakeDegreeDays+WithinLakeGLD
+Xformula
+
+### The taxa pres/abs data matrix
+Ydata=as.matrix(JSDM.subdata[,c(21:61)])
+head(Ydata)
+dim(Ydata)
+
+### Defining lake ids (DOWLKNUM) as a random factor
+DOWLKNUM=as.factor(JSDM.subdata$DOWLKNUM)
+sample.id=as.factor(1:length(JSDM.subdata$DOWLKNUM))
+studyDesign.subdata=data.frame(sample=sample.id, plot=DOWLKNUM)
+head(studyDesign.subdata)
+rL.DOWLKNUM = HmscRandomLevel(units = levels(studyDesign.subdata$plot))
+rL.DOWLKNUM
+ranlevels = list("plot" = rL.DOWLKNUM)
+ranlevels
+
+
+SXY_10PerTaxa=cbind(studyDesign.subdata,Xdata,Ydata)
+head(SXY_10PerTaxa)
+write_csv(SXY_10PerTaxa,"Data/SXY_JSDMdata_10per.csv")
+
+### Model 1 : A very basic model, no random effects
+DOW_Top10PerTaxa_simplemodel = Hmsc(Y=Ydata, XData = Xdata,
+                                    XFormula = Xformula,
+                                    studyDesign = studyDesign.subdata,
+                                    distr = "probit")
+
+DOW_Top10PerTaxa_simplemodel
+save(DOW_Top10PerTaxa_simplemodel,file="DOW_Top10PerTaxa_unfittedsimplemodel.Rdata")
+
+### Model 2: Model with lake ids as random effects
+DOW_Top10PerTaxa_model = Hmsc(Y=Ydata, XData = Xdata,
+                           XFormula = Xformula,
+                           studyDesign = studyDesign.subdata,
+                           ranLevels  = ranlevels,
+                           distr = "probit")
+
+DOW_Top10PerTaxa_model
 
 load.Rdata("DOW_Top10PerTaxa_unfittedmodel.Rdata", "DOW_Top10PerTaxa_unfittedmodel")
 ufm=DOW_10PerTaxa_unfittedmodel
